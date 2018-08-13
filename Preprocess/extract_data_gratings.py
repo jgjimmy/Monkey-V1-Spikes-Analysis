670db3ed5abb59648076389f53501c023adb6228
Jimmy Gaudreault <30395940+jgjimmy@users.noreply.github.com>
2018-08-13 11:38:31 +0900

    First upload

--- Preprocess/extract_data_gratings.py
+++ Preprocess/extract_data_gratings.py
@@ -4,19 +4,23 @@ Extracts the data from MATLAB files to pickle Python files.
 
 ---
 
-This code uses approximate inference methods for State-Space Analysis of Spike
-Correlations previously developped (Shimazaki et al. PLoS Comp Bio 2012) to 
-analyze monkey V1 neurons spiking data (Smith and Kohn, Journal of Neuroscience
-2008). We acknowledge Thomas Sharp and Christian Donner respectively for
-providing the code for exact inference (from repository 
+This code uses State-space Model of Time-varying Neural Interactions previously
+developed (Shimazaki et al. PLoS Comp Biol 2012; Donner et al. PLoS Comp Biol
+2017) to analyze monkey V1 neurons spiking data (Smith and Kohn, Journal of
+Neuroscience 2008). We acknowledge Thomas Sharp and Christian Donner
+respectively for providing codes for the inference method (from repository
 <https://github.com/tomxsharp/ssll> or
-<http://github.com/shimazaki/dynamic_corr> for Matlab code) and approximation 
+<http://github.com/shimazaki/dynamic_corr> for Matlab), and its approximation
 methods (from repository <https://github.com/christiando/ssll_lib>).
 
-In this library we use the existing codes to analyze the contributions of
-pairwise interactions to macroscopic properties of neural populations and
-stimulus coding of monkey V1 neurons. For details see: 
-<https://arxiv.org/abs/1807.08900>.
+In this library, we use the existing codes to analyze contributions of pairwise
+interactions to macroscopic properties of neural populations and stimulus coding
+of monkey V1 neurons. For details see:
+
+Jimmy Gaudreault and Hideaki Shimazaki. (2018) State-space analysis of an Ising
+model reveals contributions of pairwise interactions to sparseness, fluctuation,
+and stimulus coding of monkey V1 neurons. arXiv:1807.08900.
+<https://arxiv.org/abs/1807.08900>
 
 Copyright (C) 2018
 
@@ -64,5 +68,3 @@ snr.append(mat3['data']['SNR'].item(0))
 f = open('gratings_snr', 'wb')
 pickle.dump(snr ,f)
 f.close()
-
-
